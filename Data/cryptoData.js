47b187aa44fbc6594345b0388118d1873f491579
arun2604 <54104684+arun2604@users.noreply.github.com>
2022-09-10 09:35:24 +0530

    Create cryptoData.js

---
+++ Data/cryptoData.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+export const getCryptoData = async() => {
+    try{
+        const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=7d');
+        const data = res.data;
+        return data
+    }catch(error){
+        console.log(error.message)
+    }
+}
